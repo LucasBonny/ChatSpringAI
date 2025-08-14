dac5d8b1315132349fa7ab08e8c4cd76b19a571e
Lucas Bonny <lucas@gunthercloud.com.br>
2025-08-14 11:44:03 -0300

    Arquivos do projeto

---
+++ src/main/java/br/com/gunthercloud/ChatSpringAI/ChatSpringAiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gunthercloud.ChatSpringAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatSpringAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatSpringAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gunthercloud/ChatSpringAI/ChatSpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gunthercloud.ChatSpringAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatSpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
